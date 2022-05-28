42f1e1c787e608a8d49f06734ec993d64425c601
Anibal <anibal.gatica@mail.udp.cl>
2022-05-28 10:05:53 +0100

    test 3

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText("Empty CRA Test Number 2");
+  const linkElement = screen.getByText("Empty CRA Test Number 3");
   expect(linkElement).toBeInTheDocument();
 });
